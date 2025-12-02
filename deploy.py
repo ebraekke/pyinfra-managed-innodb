58976b410319fdeb03e55b56848cc78cae3b2e63
espenbr <espenbr@Mac.home>
2025-12-02 08:54:54 +0100

    cleanup before leaving ?

--- deploy.py
+++ deploy.py
@@ -3,30 +3,42 @@
 from pyinfra.api.deploy import deploy
 from pyinfra.api import DeployError
 from pyinfra import host
-from pyinfra.operations import server
+from pyinfra.operations import dnf, yum, server, files
 
-REQUIRED_ENV_VARIABLES = [
-    "ssh_config_file",
-]
+MYSQL_VERSION = host.data.mysql_version
 
-@deploy("Check required environment variables")
-def check_required_environment(state, host):
-    missing = []
-    for var in REQUIRED_ENV_VARIABLES:
-        if not getattr(host.data, var):
-            missing.append(var)
-
-    if missing:
-        raise DeployError(
-            f"Missing required environment variables: {', '.join(missing)}"
-        )
-
-
-# Register the check as an operation (no parentheses!)
-check_required_environment
-
-# Example real operation (MySQL install, firewall, etc.)
-server.shell(
-    name="Just a placeholder operation",
-    commands=["echo All environment variables OK!"],
+dnf.packages(
+    name="Remove builtin MySQL",
+    packages=["mysql", "mysql-shell"],
+    present=False,
+    _sudo=True,
 )
+
+yum.key(
+    name="Import MySQL key",
+    src="https://repo.mysql.com/RPM-GPG-KEY-mysql-2023",
+    _sudo=True
+)
+
+yum.rpm(
+    name="Install MySQL Community repo",
+    src="https://dev.mysql.com/get/mysql84-community-release-el8-2.noarch.rpm",
+#    src="https://dev.mysql.com/get/mysql84-community-release-$(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/')-1.noarch.rpm",
+    present=True, 
+    _sudo=True
+)
+
+# For RHEL/CentOS systems using 'yum' (typically RHEL 7 and older)
+server.shell(
+    name="Clean Yum metadata cache to force refresh",
+    commands=["yum clean metadata", "yum makecache"],
+    _Sudo=True
+)
+
+#yum.packages(
+#    name="Install mysql",
+#    packages=["mysql-shell=8.4.7", "mysql-server=8.4.7"],
+#    update=True,
+#    _sudo=True,
+#)
+
