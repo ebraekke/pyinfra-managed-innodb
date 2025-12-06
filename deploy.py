0381c2a646df683a2890e9013d66deb55d06dd64
espenbr <espenbr@Mac.home>
2025-12-06 19:24:10 +0100

    Restart, based on fab tests

--- deploy.py
+++ deploy.py
@@ -4,21 +4,49 @@ from pyinfra.api.deploy import deploy
 from pyinfra.api import DeployError
 from pyinfra import host
 from pyinfra.operations import dnf, yum, server, files
+from pyinfra.facts import server as server_facts
 
 MYSQL_VERSION = host.data.mysql_version
 
-dnf.packages(
-    name="Remove builtin MySQL",
-    packages=["mysql", "mysql-shell"],
-    present=False,
-    _sudo=True,
-)
+from pyinfra.operations import server
 
-yum.key(
-    name="Import MySQL key",
-    src="https://repo.mysql.com/RPM-GPG-KEY-mysql-2023",
+server.shell(
+    name="Disable mysql dnf module on el8",
+    commands=[
+        "if [ $(uname -r | sed 's/^.*\(el[0-9]\+\).*$/\1/') == \"el8\" ]; then  dnf -y module disable mysql; fi"
+    ],
     _sudo=True
 )
+
+server.shell(
+    name="Import MySQL key",
+    commands=[
+        "rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023",
+    ],
+    _sudo=True
+)
+
+# Define the local path to your script and the remote destination
+script_local_path = "scripts/linux/rpm_install.sh"
+script_remote_path = "/tmp/rpm_install.sh"
+
+# 1. Upload the script to the target host
+files.put(
+    name="Upload script to target host",
+    src=script_local_path,
+    dest=script_remote_path,
+    mode="644",  # don't need to be executable
+)
+
+# 2. Execute the uploaded script on the target host
+server.shell(
+    name="Execute script on target host",
+    commands=[
+        f"sh {script_remote_path}",  # Or python, sh, etc., depending on your script
+    ],
+    _sudo=True
+)
+"""
 
 yum.rpm(
     name="Install MySQL Community repo",
@@ -28,6 +56,7 @@ yum.rpm(
     _sudo=True
 )
 
+
 # For RHEL/CentOS systems using 'yum' (typically RHEL 7 and older)
 server.shell(
     name="Clean Yum metadata cache to force refresh",
@@ -35,10 +64,12 @@ server.shell(
     _Sudo=True
 )
 
-#yum.packages(
-#    name="Install mysql",
-#    packages=["mysql-shell=8.4.7", "mysql-server=8.4.7"],
-#    update=True,
-#    _sudo=True,
-#)
 
+yum.packages(
+    name="Ensure mysql and mysqlsh are present",
+    packages=["mysql-shell=8.4.7", "mysql-community-server=8.4.7"],
+    present=True,
+    _sudo=True,
+)
+
+"""
